2899c0b440afc9e78cd5aec9e03691fb272e56de
liuke <liuke.rd@bytedance.com>
2021-07-21 18:23:57 +0800

    初始化工程

---
+++ FileCommander/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FileCommander
+//
+//  Created by liuke on 2021/7/21.
+//
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+        let v = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        v.backgroundColor = UIColor.red
+        view.addSubview(v)
+    }
+
+
+
+}
---
+++ Pods/Target Support Files/Pods-FileCommanderTests/Pods-FileCommanderTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FileCommanderTests : NSObject
+@end
+@implementation PodsDummy_Pods_FileCommanderTests
+@end
